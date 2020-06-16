80fae0c50f67901703b4cbc35b65aa57f6b4eedf
Brandon Lantz <brandon.lantz@icloud.com>
2020-06-16 07:35:56 -0600

    Add files

---
+++ answer.js
@@ -0,0 +1,115 @@
+var stopwords = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+var isStopword = { }
+stopwords.map(stopword => isStopword[stopword] = true)
+
+function sentenceSimilarities(sentence1, sentence2) {
+  var sent1terms = sentence1.contractions().expand().all().terms()
+  var sent2terms = sentence2.contractions().expand().all().terms()
+
+  return sent1terms.map(term1 => {
+    var word1 = term1.text('clean').replace(/[^a-z0-9]/, '')
+    var bestTerm
+    var bestSim = 0
+
+    var word1vec = wordVecs[word1]
+    sent2terms.some(term2 => {
+      var word2 = term2.text('clean').replace(/[^a-z0-9]/, '')
+
+      if (word2 === word1) {
+        bestTerm = term2
+        bestSim = 1
+
+        return true
+      }
+
+      var word2vec = wordVecs[word2]
+
+      var similarity = 0
+      if (word1vec && word2vec) {
+        similarity = Word2VecUtils.getCosSim(word1vec, word2vec)
+      } else {
+        similarity = stringSimilarity.compareTwoStrings(word1, word2)
+      }
+
+      if (similarity > bestSim) {
+        bestTerm = term2
+        bestSim = similarity
+      }
+    })
+
+    return {
+      term1,
+      term2: bestTerm,
+      score: bestSim
+    }
+  })
+}
+
+function findAnswer(question, answers) {
+  question = nlp(question)
+  answers = answers.map(text => nlp(text))
+
+  question.debug()
+  var questionWord = question.matchOne('#QuestionWord').text('clean').replace(/[^a-z]/, '')
+  var questionType
+  if (questionWord === 'how' && question.has('how (much|many)')) {
+    questionType = 'how much'
+  } else if (questionWord === 'who' || questionWord === 'whom') {
+    questionType = 'who'
+  } else if (questionWord === 'when') {
+    questionType = 'when'
+  } else if (questionWord === 'where') {
+    questionType = 'where'
+  }
+  console.log('type', questionType)
+
+  var rankedAnswers = answers.map((answer, i) => {
+    var similarities = sentenceSimilarities(question, answer)
+    var weightData = []
+    var weightedSim = similarities.reduce((prev, curr) => {
+      var score = curr.score
+
+      var weight = 0.5
+      if (isStopword[curr.term1.text('clean').replace(/[^a-z]/, '')]) weight = 0.25
+      else if (curr.term1.has('#Noun') || curr.term1.has('#Verb')) weight = 1
+
+      weightData.push({
+        text: curr.term1.text('clean'),
+        match: curr.term2 ? curr.term2.text('clean') : undefined,
+        score, weight
+      })
+
+      return prev + score * weight
+    }, 0) / similarities.length
+
+    var questionAnswerMult = 1
+    if (questionType) {
+      questionAnswerMult = 0.5
+      if (questionType === 'how much' && answer.match('#Value').not('#Date').length > 0) {
+        questionAnswerMult = 1
+      } else if (questionType === 'who' && answer.has('#Person')) {
+        questionAnswerMult = 1
+      } else if (questionType === 'when' && answer.has('(#Time|#Date)')) {
+        questionAnswerMult = 1
+      } else if (questionType === 'where' && answer.has('#Place')) {
+        questionAnswerMult = 1
+      }
+    }
+
+    var score = weightedSim * questionAnswerMult
+
+    return {
+      index: i,
+      score,
+      weightData: {
+        similarities: weightData,
+        questionType,
+        answerScore: questionAnswerMult 
+      },
+      text: answer.text()
+    }
+  })
+  rankedAnswers.sort((a, b) => b.score - a.score)
+
+  return rankedAnswers
+}
---
+++ script.js
@@ -0,0 +1,38 @@
+var promptInput = document.getElementById('prompt')
+var questionInput = document.getElementById('question')
+var output = document.getElementById('output')
+
+promptInput.value = `
+On May 3, 1901, downtown Jacksonville was ravaged by a fire that started as a kitchen fire. Spanish moss at a nearby mattress factory was quickly engulfed in flames and enabling the fire to spread rapidly. In just eight hours, it swept through 146 city blocks, destroyed over 2,000 buildings, left about 10,000 homeless and killed 7 residents. The Confederate Monument in Hemming Park was one of the only landmarks to survive the fire. Governor Jennings declare martial law and sent the state militia to maintain order. On May 17 municipal authority resumed in Jacksonville. It is said the glow from the flames could be seen in Savannah, Georgia, and the smoke plumes seen in Raleigh, North Carolina. Known as the "Great Fire of 1901", it was one of the worst disasters in Florida history and the largest urban fire in the southeastern United States. Architect Henry John Klutho was a primary figure in the reconstruction of the city. The first multi-story structure built by Klutho was the Dyal-Upchurch Building in 1902. The St. James Building, built on the previous site of the St. James Hotel that burned down, was built in 1912 as Klutho's crowning achievement.
+`.trim()
+questionInput.value = `
+How many buildings were destroyed?
+`.trim()
+
+function showAnswer() {
+  var prompt = promptInput.value
+  var question = questionInput.value
+  console.log(prompt)
+  console.log(question)
+
+  var nlpPrompt = nlp(prompt)
+  var promptSentences = nlpPrompt.sentences().json().map(sentence => sentence.text)
+
+  var answers = findAnswer(question, promptSentences)
+  var maxScore = answers[0].score
+
+  var sentences = Array(answers.length).fill(undefined)
+  answers.forEach(answer => {
+    var newEl = document.createElement('span')
+    var normScore = answer.score / maxScore
+    newEl.style.background = `rgba(255, 255, 0, ${normScore * normScore})`
+    newEl.innerText = answer.text + ' '
+    sentences[answer.index] = newEl
+  })
+
+  output.innerHTML = ''
+  sentences.forEach(sentEl => output.appendChild(sentEl))
+
+  console.log(answers)
+}
+showAnswer()
